4b036ebb09383c44fb8328de850808e11360fa4b
EMANALSHALABI <107142552+EMANALSHALABI@users.noreply.github.com>
2024-01-23 11:32:23 +0300

    Add files via upload

---
+++ startbootstrap-agency-gh-pages/js/click-scroll.js
@@ -0,0 +1,74 @@
+//jquery-click-scroll
+//by syamsul'isul' Arifin
+
+var sectionArray = [1, 2, 3, 4, 5];
+
+$.each(sectionArray, function (index, value) {
+
+    $(document).scroll(function () {
+        var offsetSection = $('#' + 'section_' + value).offset().top - 94;
+        var docScroll = $(document).scrollTop();
+        var docScroll1 = docScroll + 1;
+
+
+        if (docScroll1 >= offsetSection) {
+            $('.navbar-nav .nav-item .nav-link').removeClass('active');
+            $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
+            $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+            $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+        }
+
+    });
+
+    $('.click-scroll').eq(index).click(function (e) {
+        var offsetClick = $('#' + 'section_' + value).offset().top - 94;
+        e.preventDefault();
+        $('html, body').animate({
+            'scrollTop': offsetClick
+        }, 300)
+    });
+
+});
+const privacyLink = document.getElementById('privacy-link');
+privacyLink.addEventListener('click', () => {
+    window.location.href = 'page.html';
+});
+const imp = document.getElementById('imp');
+privacyLink.addEventListener('click', () => {
+    window.location.href = 'impressum.html';
+});
+window.addEventListener('DOMContentLoaded', function () {
+    var logoContainer = document.getElementById('logo-container');
+    var hasLogoShown = localStorage.getItem('hasLogoShown');
+
+    if (!hasLogoShown) {
+        logoContainer.classList.remove('hidden');
+        localStorage.setItem('hasLogoShown', true);
+
+        setTimeout(function () {
+            logoContainer.classList.add('hidden');
+            localStorage.clear();
+
+        }, 3000);
+    } else {
+        logoContainer.style.display = 'none';
+        localStorage.clear();
+
+    }
+});
+
+$(".nav-link").on("shown.bs.tab", function (e) {
+    var href = $(this).attr("data-bs-target");
+    $("html, body").animate(
+        {
+            scrollTop: $(href).offset().top
+        },
+        "slow"
+    );
+});
+
+$(document).ready(function () {
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
+    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
+    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
+});
