4bbe0e8451e53c7253d416d314be5ba263a0cdfc
EMANALSHALABI <107142552+EMANALSHALABI@users.noreply.github.com>
2024-01-29 12:39:00 +0100

    Update click-scroll.js

--- startbootstrap-agency-gh-pages/js/click-scroll.js
+++ startbootstrap-agency-gh-pages/js/click-scroll.js
@@ -1,24 +1,24 @@
 //jquery-click-scroll
 //by syamsul'isul' Arifin
 
-// var sectionArray = [1, 2, 3, 4, 5];
+var sectionArray = [1, 2, 3, 4, 5];
 
-// $.each(sectionArray, function (index, value) {
+$.each(sectionArray, function (index, value) {
 
-//     $(document).scroll(function () {
-//         var offsetSection = $('#' + 'section_' + value).offset().top - 94;
-//         var docScroll = $(document).scrollTop();
-//         var docScroll1 = docScroll + 1;
+    $(document).scroll(function () {
+        var offsetSection = $('#' + 'section_' + value).offset().top - 94;
+        var docScroll = $(document).scrollTop();
+        var docScroll1 = docScroll + 1;
 
 
-//         if (docScroll1 >= offsetSection) {
-//             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-//             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
-//             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-//             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-//         }
+        if (docScroll1 >= offsetSection) {
+            $('.navbar-nav .nav-item .nav-link').removeClass('active');
+            $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
+            $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+            $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+        }
 
-//     });
+    });
 
     $('.click-scroll').eq(index).click(function (e) {
         var offsetClick = $('#' + 'section_' + value).offset().top - 94;
@@ -49,7 +49,7 @@ window.addEventListener('DOMContentLoaded', function () {
             logoContainer.classList.add('hidden');
             localStorage.clear();
 
-        }, 3000);
+        }, 300);
     } else {
         logoContainer.style.display = 'none';
         localStorage.clear();
